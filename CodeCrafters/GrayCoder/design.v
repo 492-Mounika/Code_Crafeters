3040861a059fddcd9fb764f94616c073b45385c7
Mounika <mounika@linux.com>
2025-08-02 15:35:14 +0530

    Modifed GrayCoder

--- CodeCrafters/GrayCoder/design.v
+++ CodeCrafters/GrayCoder/design.v
@@ -1,18 +1,19 @@
 module GrayCoder(
-    input wire valid,
-    input wire [7:0] data,
-    output reg [7:0] gcode
+    input wire valid,                 //New Input  
+    input wire [7:0] data,            //Data input
+    output reg [7:0] gcode            //Gray code output
 );
 integer i;
+//main block
 always @(*) begin
-    if (valid) begin
+    if (valid) begin                  //Starts executing when new input is given
         gcode[7] = data[7];
         for(i = 6;i>=0;i--) begin
-            gcode[i] = data[i]^data[i+1];
+            gcode[i] = data[i]^data[i+1];   //Graycode logic
         end
     end
     else begin
-        gcode = 8'b00000000;
+        gcode = 8'b00000000;    //gcode sets to zero when no input is given
     end
 end
 endmodule
